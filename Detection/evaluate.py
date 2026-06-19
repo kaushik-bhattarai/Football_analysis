46e3809193bdbcf394d5af8b54353cf17bdb5ace
kaushik-bhattarai <kaushikbhattarai111@gmail.com>
2026-06-19 10:52:14 +0545

    Initial commit

--- Detection/evaluate.py
+++ Detection/evaluate.py
@@ -1,9 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO("/home/kaushik/Football_Detection/runs/detect/train-4/weights/best.pt")
+model = YOLO("/home/kaushik/Football_Detection/runs/detect/train/weights/best.pt")
 
 val = model.val(data="/home/kaushik/Football_Detection/Datasets/football-players-detection.v20-rf-detr-m.yolo26/data.yaml", split="val",batch=4,device=0)
 print(f"Val  mAP50: {val.box.map50:.4f}")
-
-test = model.val(data="/home/kaushik/Football_Detection/Datasets/football-players-detection.v20-rf-detr-m.yolo26/data.yaml", split="test",batch=4,device=0)
-print(f"Test mAP50: {test.box.map50:.4f}")
--- Detection/train.py
+++ Detection/train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO("yolo26l.pt")
+model = YOLO("Models/yolo26l.pt")
 
 model.train(data="/home/kaushik/Football_Detection/Datasets/football-players-detection.v20-rf-detr-m.yolo26/data.yaml",
             epochs=50,
